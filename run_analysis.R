8a294276843cd5978be3449ff448421213826922
LaraGalimberti <58987739+LaraGalimberti@users.noreply.github.com>
2022-01-04 14:29:17 +0100

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,75 @@
+# run_analysis.R
+
+#0. prepare LIBs and install packages
+install.packages("reshape2")
+library(reshape2)
+
+
+#1. get dataset from web
+getwd()
+rawDataDir <- "./Getting_cleaning_data"
+rawDataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+rawDataFilename <- "Final_project.zip"
+rawDataDFn <- paste(rawDataDir, "/", rawDataFilename, sep = "")
+dataDir <- "./data"
+
+if (!file.exists(rawDataDir)) {
+     dir.create(rawDataDir)
+     download.file(url = rawDataUrl, destfile = rawDataDFn)
+}
+if (!file.exists(dataDir)) {
+     dir.create(dataDir)
+     unzip(zipfile = rawDataDFn, exdir = dataDir)
+}
+
+
+#2. merge {train, test} data set
+# refer: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+# train data
+x_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/X_train.txt"))
+y_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/Y_train.txt"))
+s_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/subject_train.txt"))
+
+# test data
+x_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/X_test.txt"))
+y_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/Y_test.txt"))
+s_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/subject_test.txt"))
+
+# merge {train, test} data
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+s_data <- rbind(s_train, s_test)
+
+
+#3. load feature & activity info
+# feature info
+feature <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/features.txt"))
+
+# activity labels
+a_label <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/activity_labels.txt"))
+a_label[,2] <- as.character(a_label[,2])
+
+# extract feature cols 
+selectedCols <- grep("-(mean|std).*", as.character(feature[,2]))
+selectedColNames <- feature[selectedCols, 2]
+
+#replacements
+selectedColNames <- gsub("-mean", "Mean", selectedColNames)
+selectedColNames <- gsub("-std", "Std", selectedColNames)
+selectedColNames <- gsub("[-()]", "", selectedColNames)
+
+
+#4. extract data by cols and using descriptive name
+x_data <- x_data[selectedCols]
+allData <- cbind(s_data, y_data, x_data)
+colnames(allData) <- c("Subject", "Activity", selectedColNames)
+
+allData$Activity <- factor(allData$Activity, levels = a_label[,1], labels = a_label[,2])
+allData$Subject <- as.factor(allData$Subject)
+
+
+#5. create tidy data set
+meltedData <- melt(allData, id = c("Subject", "Activity"))
+tidyData <- dcast(meltedData, Subject + Activity ~ variable, mean)
+
+write.table(tidyData, "./tidy_dataset_fin.txt", row.names = FALSE, quote = FALSE)
